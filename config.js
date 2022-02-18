e90794257f4afac744b2538f2236fbdf7eee91b0
Chingkheinganba Irungbam <99580046+Chingkheinganba06@users.noreply.github.com>
2022-02-19 00:23:59 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-global.owner = ['919002370263','917622836211'] // Put your number here
+global.owner = ['918414802914','17059996562'] // Put your number here
 global.mods = [] // Want some help?
 global.prems = [] // Premium user has unlimited limit
 global.APIs = { // API Prefix
